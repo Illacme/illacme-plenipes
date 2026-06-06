c28a7a3feabb6a144ff424fe0bc1a6ef820dab51
Illacme <wqbyc@qq.com>
2026-06-06 12:13:15 +0800

    feat(theme): 将通用主题配置文件及源码直接纳入主项目追踪

---
+++ themes/universal/theme.options.js
@@ -0,0 +1,8 @@
+// 🚀 [V88.0 Live Hot-Reload] 自动生成的主题选项常量，请勿手动编辑
+export const themeOptions = {
+  "site_name": "Borealis realm",
+  "footer_copyright": "© 2026 Sovereign Writer. All Rights Reserved.",
+  "author": "Sovereign Writer",
+  "site_description": "这是一个主权出版版图节点。"
+};
+export default themeOptions;
